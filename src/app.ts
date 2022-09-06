4e514982c4840cdfb11c344730193a695c1b6167
Orlando Monrroy <monroyorlando.36@gmail.com>
2022-09-06 19:02:53 -0400

    initial config

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+const isDev = process.env.NODE_ENV === "development";
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express and TypescriptServer");
+});
+
+(async () => {
+  await db
+    .sync({ alter: isDev })
+    .then(() => console.log("Connected to database..."))
+    .catch((error: any) => console.log(error));
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at https://localhost:${port}`);
+  });
+})();
